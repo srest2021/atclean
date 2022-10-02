91a0aef3c3f4579c2dc9fbca605ea52c8fe5c04a
Sofia Rest <srest2021@gmail.com>
2022-10-01 21:08:17 -0400

    added mjd range option for detecting precursor bumps

--- clean_atlas_lc.py
+++ clean_atlas_lc.py
@@ -79,6 +79,8 @@ class clean_atlas_lc():
 		self.detect_bumps = False
 		self.gaussian_sigma = None
 		self.appmags = None
+		self.start_mjd = None
+		self.end_mjd = None
 	
 	# define command line arguments
 	def define_args(self, parser=None, usage=None, conflict_handler='resolve'):
@@ -209,6 +211,8 @@ class clean_atlas_lc():
 			print(f'# Applying to control light curves in order to establish detection limit: {self.apply_to_controls}')
 			self.gaussian_sigma = float(cfg['Detecting bumps settings']['gaussian_sigma'])
 			print(f'# Searching for pre-SN bumps with a sigma of {self.gaussian_sigma:0.2f} days')
+			
+			# simulated gaussian settings
 			if not(args.sim_gaussian is None):
 				print(f'# Adding simulated gaussian pre-SN bump to SN light curve: True')
 				if ',' in args.sim_gaussian[1]:
@@ -217,6 +221,14 @@ class clean_atlas_lc():
 				else:
 					self.appmags = [args.sim_gaussian[1]]
 					print(f'## Only one magnitude input: {self.appmags}')
+			
+			# custom MJD range for bump detection
+			if not(cfg['Detecting bumps settings']['start_mjd'] == 'None'):
+				self.start_mjd = float(cfg['Detecting bumps settings']['start_mjd'])
+				print('# Will start bump detection at MJD={self.start_mjd}')
+			if not(cfg['Detecting bumps settings']['end_mjd'] == 'None'):
+				print('# Will end bump detection at MJD={self.end_mjd}')
+				self.end_mjd = float(cfg['Detecting bumps settings']['end_mjd'])
 
 	# helper function for get_baseline_regions()
 	def get_Ndays(self, SN_region_index):
@@ -966,9 +978,18 @@ class clean_atlas_lc():
 
 		return lc, avglc, output
 
-	# add simulated bump if necessary and apply rolling gaussian weighted sum to light curve
+	# add simulated bump if necessary and apply rolling gaussian weighted ssm to light curve
 	def apply_gaussian(self, avglc, control_index=0, simparams=None, filt=None):
-		bad_ix = avglc.lcs[control_index].ix_unmasked('Mask',self.flags['avg_badday'])
+		if self.start_mjd is None:
+			self.start_mjd = avglc.lcs[control_index].t['MJDbin'].iloc[0]
+		if self.end_mjd is None:
+			self.end_mjd = avglc.lcs[control_index].t['MJDbin'].iloc[-1]
+		ix = avglc.lcs[control_index].ix_inrange(colnames=['MJDbin'],lowlim=self.start_mjd, uplim=self.end_mjd)
+		if len(ix) <= 0:
+			raise RuntimeError('ERROR: specified MJD range less than or equal to 0!')
+		print(f'# Applying detection to specified MJD bin range: {avglc.lcs[control_index].t["MJDbin"].iloc[ix[0]]} to  {avglc.lcs[control_index].t["MJDbin"].iloc[ix[-1]]}')
+
+		good_ix = avglc.lcs[control_index].ix_unmasked('Mask',self.flags['avg_badday'])
 
 		# make sure there are no lingering simulations
 		dropcols=[]
@@ -978,32 +999,32 @@ class clean_atlas_lc():
 		if len(dropcols) > 0:
 			avglc.lcs[control_index].t.drop(columns=dropcols,inplace=True)
 
-		avglc.lcs[control_index].t['SNR'] = 0.0
-		avglc.lcs[control_index].t.loc[bad_ix,'SNR'] = avglc.lcs[control_index].t.loc[bad_ix,'uJy']/avglc.lcs[control_index].t.loc[bad_ix,'duJy']
+		avglc.lcs[control_index].t.loc[ix, 'SNR'] = 0.0
+		avglc.lcs[control_index].t.loc[good_ix,'SNR'] = avglc.lcs[control_index].t.loc[good_ix,'uJy']/avglc.lcs[control_index].t.loc[good_ix,'duJy']
 
 		if not(simparams is None):
 			peakMJDs = simparams['sim_peakMJD'].split(',')
 			
 			# get the simulated gaussian
-			mjds = avglc.lcs[control_index].t.loc[bad_ix,'MJD']
-			avglc.lcs[control_index].t.loc[bad_ix,'uJysim'] = avglc.lcs[control_index].t.loc[bad_ix,'uJy']
-			avglc.lcs[control_index].t['simLC'] = 0.0
+			mjds = avglc.lcs[control_index].t.loc[good_ix,'MJD']
+			avglc.lcs[control_index].t.loc[good_ix,'uJysim'] = avglc.lcs[control_index].t.loc[good_ix,'uJy']
+			avglc.lcs[control_index].t.loc[ix,'simLC'] = 0.0
 			for peakMJD in peakMJDs:
 				peakMJD = float(peakMJD)
 				print(f'## Adding simulated gaussian at peak MJD {peakMJD:0.2f} with apparent magnitude {simparams["sim_appmag"]:0.2f}, sigma- of {simparams["sim_sigma_minus"]:0.2f}, and sigma+ of {simparams["sim_sigma_plus"]:0.2f}')
 
 				# get simulated gaussian flux and add to light curve flux
 				simflux = gauss2lc(mjds, peakMJD, simparams['sim_sigma_minus'], simparams['sim_sigma_plus'], app_mag=simparams['sim_appmag'])
-				avglc.lcs[control_index].t.loc[bad_ix,'uJysim'] += simflux
+				avglc.lcs[control_index].t.loc[good_ix,'uJysim'] += simflux
 
 				# get the simulated lc for all MJDs
-				simflux_all = gauss2lc(avglc.lcs[control_index].t['MJDbin'], peakMJD, simparams['sim_sigma_minus'], simparams['sim_sigma_plus'], app_mag=simparams['sim_appmag'])
-				avglc.lcs[control_index].t['simLC'] += simflux_all
+				simflux_all = gauss2lc(avglc.lcs[control_index].t.loc[ix,'MJDbin'], peakMJD, simparams['sim_sigma_minus'], simparams['sim_sigma_plus'], app_mag=simparams['sim_appmag'])
+				avglc.lcs[control_index].t.loc[ix,'simLC'] += simflux_all
 
 			# make sure all bad rows have SNRsim = 0.0 so they have no impact on the rolling SNRsum
-			avglc.lcs[control_index].t['SNRsim'] = 0.0
+			avglc.lcs[control_index].t.loc[ix,'SNRsim'] = 0.0
 			# include simflux in the SNR
-			avglc.lcs[control_index].t.loc[bad_ix,'SNRsim'] = avglc.lcs[control_index].t.loc[bad_ix,'uJysim']/avglc.lcs[control_index].t.loc[bad_ix,'duJy']
+			avglc.lcs[control_index].t.loc[good_ix,'SNRsim'] = avglc.lcs[control_index].t.loc[good_ix,'uJysim']/avglc.lcs[control_index].t.loc[good_ix,'duJy']
 
 		gaussian_sigma = round(self.gaussian_sigma/self.mjd_bin_size)
 		windowsize = int(6*gaussian_sigma)
@@ -1012,28 +1033,24 @@ class clean_atlas_lc():
 
 		# calculate the rolling SNR sum
 		
-		dataindices = np.array(range(len(avglc.lcs[control_index].t)) + np.full(len(avglc.lcs[control_index].t), halfwindowsize))
+		dataindices = np.array(range(len(avglc.lcs[control_index].t.loc[ix])) + np.full(len(avglc.lcs[control_index].t.loc[ix]), halfwindowsize))
 		
-		temp = pd.Series(np.zeros(len(avglc.lcs[control_index].t) + 2*halfwindowsize), name='SNR', dtype=np.float64)
-		temp[dataindices] = avglc.lcs[control_index].t['SNR']
+		temp = pd.Series(np.zeros(len(avglc.lcs[control_index].t.loc[ix]) + 2*halfwindowsize), name='SNR', dtype=np.float64)
+		temp[dataindices] = avglc.lcs[control_index].t.loc[ix,'SNR']
 		SNRsum = temp.rolling(windowsize, center=True, win_type='gaussian').sum(std=gaussian_sigma)
-		avglc.lcs[control_index].t['SNRsum'] = list(SNRsum.loc[dataindices])
+		avglc.lcs[control_index].t.loc[ix,'SNRsum'] = list(SNRsum[dataindices])
 		
-		norm_temp = pd.Series(np.zeros(len(avglc.lcs[control_index].t) + 2*halfwindowsize), name='norm', dtype=np.float64)
-		norm_temp[np.array(range(len(avglc.lcs[control_index].t)) + np.full(len(avglc.lcs[control_index].t), halfwindowsize))] = np.ones(len(avglc.lcs[control_index].t))
+		norm_temp = pd.Series(np.zeros(len(avglc.lcs[control_index].t.loc[ix]) + 2*halfwindowsize), name='norm', dtype=np.float64)
+		norm_temp[np.array(range(len(avglc.lcs[control_index].t.loc[ix])) + np.full(len(avglc.lcs[control_index].t.loc[ix]), halfwindowsize))] = np.ones(len(avglc.lcs[control_index].t.loc[ix]))
 		norm_temp_sum = norm_temp.rolling(windowsize, center=True, win_type='gaussian').sum(std=gaussian_sigma)
-		
-		avglc.lcs[control_index].t['SNRsumnorm'] = list(SNRsum.loc[dataindices] / norm_temp_sum.loc[dataindices] * max(norm_temp_sum.loc[dataindices]))
+		avglc.lcs[control_index].t.loc[ix,'SNRsumnorm'] = list(SNRsum.loc[dataindices] / norm_temp_sum.loc[dataindices] * max(norm_temp_sum.loc[dataindices]))
 
 		# calculate the rolling SNR sum for SNR with simflux
 		if not(simparams is None):
-			temp = pd.Series(np.zeros(len(avglc.lcs[control_index].t) + 2*halfwindowsize), name='SNRsim', dtype=np.float64)
-			temp[dataindices] = avglc.lcs[control_index].t['SNRsim']
+			temp = pd.Series(np.zeros(len(avglc.lcs[control_index].t.loc[ix]) + 2*halfwindowsize), name='SNRsim', dtype=np.float64)
+			temp[dataindices] = avglc.lcs[control_index].t.loc[ix,'SNRsim']
 			SNRsimsum = temp.rolling(windowsize, center=True, win_type='gaussian').sum(std=gaussian_sigma)
-			avglc.lcs[control_index].t['SNRsimsum'] = list(SNRsimsum.loc[dataindices])
-		
-		# DELETE ME
-		#avglc.save(self.output_dir, filt=filt, overwrite=self.overwrite)
+			avglc.lcs[control_index].t.loc[ix,'SNRsimsum'] = list(SNRsimsum.loc[dataindices])
 
 		return avglc
 
