b07db59048db48cf65739b008f95a8be6d083d49
Sofia Rest <srest2021@gmail.com>
2024-05-15 20:02:59 -0400

    add args

--- download.py
+++ download.py
@@ -13,9 +13,30 @@ Outputs:
 - if using TNS credentials, new or updated .txt table with TNS names, RA, Dec, and discovery dates
 """
 
-import requests
+import requests, argparse
 import pandas as pd
 from lightcurve import SnInfo, FullLightCurve
+
+# define command line arguments
+def define_args(parser=None, usage=None, conflict_handler='resolve'):
+  if parser is None:
+    parser = argparse.ArgumentParser(usage=usage, conflict_handler=conflict_handler)
+    
+  parser.add_argument('tnsnames', nargs='+', help='TNS names of the objects to download from ATLAS')
+  parser.add_argument('--coords', type=str, default=None, help='comma-separated RA and Dec of SN light curve to download')
+  parser.add_argument('--discdate', type=str, default=None, help='SN discovery date in MJD')
+  
+  parser.add_argument('-c','--controls', default=False, action='store_true', help='download control light curves in addition to transient light curve')
+  parser.add_argument('--closebright', type=str, default=None, help='comma-separated RA and Dec coordinates of a nearby bright object interfering with the light curve to become center of control light curve circle')
+  parser.add_argument('--ctrl_coords', type=str, default=None, help='file name of text file in output_dir containing table of control light curve coordinates')
+
+  parser.add_argument('-f','--cfg_filename', default='settings.ini', type=str, help='file name of ini file with settings for this class')
+  parser.add_argument('-l', '--lookbacktime_days', default=None, type=int, help='lookback time (days)')
+  parser.add_argument('--mjd_min', default=None, type=float, help='minimum MJD to download')
+  parser.add_argument('--mjd_max', default=None, type=float, help='maximum MJD to download')
+  parser.add_argument('-o','--overwrite', default=False, action='store_true', help='overwrite existing file with same file name')
+
+  return parser
 
 class DownloadLoop:
   def __init__(self, args):
@@ -38,4 +59,7 @@ class DownloadLoop:
     if headers is None: 
       raise RuntimeError('ERROR: No token header!')
     
-    
+if __name__ == "__main__":
+  args = define_args().parse_args()
+  download = DownloadLoop(args)
+  download.loop()
