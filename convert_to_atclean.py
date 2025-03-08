7407706d086e65ead038c2dbfc585cb7ee9eef05
Sofia Rest <srest2021@gmail.com>
2025-03-08 13:57:37 -0500

    add config paramaters for ATLAS, Rubin, and TESS light curves

--- convert_to_atclean.py
+++ convert_to_atclean.py
@@ -3,14 +3,15 @@
 """
 Convert existing non-ATLAS files into ATClean-readable files.
 
-Additional input/output directories needed: 
-- raw TESS input (just read, don't touch)
-- ATClean readable (output by convert.py, sninfo.txt)
-- ATClean output (cleaned, binned, efficiencies) 
-
 Worry about:
 - column names
 - possible filters
 - filename formatting
 """
 
+from download import load_config
+from lightcurve import LightCurve
+
+if __name__ == "__main__":
+    args = define_args().parse_args()
+    config = load_config(args.config_file)
